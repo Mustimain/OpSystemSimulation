eba104dfc5bc2fb9137b774c6f52f42c29627aa8
Mustafa Ceylan <mustafaceylanmain@gmail.com>
2023-04-22 15:55:02 +0300

    Initial Commit

---
+++ OpSystemProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpSystemProject
+//
+//  Created by Mustafa Ceylan on 22.04.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
